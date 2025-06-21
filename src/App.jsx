68183abff60c209d361abff90d16934aa5f78870
Jesus Hernan <jesushernan3@gmail.com>
2025-06-21 09:02:49 -0300

    primer comit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 class="text-3xl font-bold underline">Creisi Crmode Bitches</h1>
+        <p className="bg-amber-300">don forget</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
